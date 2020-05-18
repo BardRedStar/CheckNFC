6a29180207fca49657776230556794175bcecd24
BardRedStar <den.kovalev999@gmail.com>
2020-05-18 14:05:27 +0700

    Add logic for NFC check.

--- CheckNFC/ViewController.swift
+++ CheckNFC/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
+    // MARK: - Outlets
+
+    @IBOutlet private var textLabel: UILabel!
+
+    // MARK: - Properties and variables
+
+    private var nfcSession: NFCNDEFReaderSession?
+
+    // MARK: - UI Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - UI Callbacks
+
+    @IBAction private func checkNFCAction(_ sender: Any) {
+        nfcSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.begin()
+    }
+}
+
+extension ViewController: NFCNDEFReaderSessionDelegate {
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        DispatchQueue.main.async { [weak self] in
+            self?.textLabel.text = error.localizedDescription
+        }
+    }
+
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        let message = messages.compactMap { String(data: $0.records.first?.payload.advanced(by: 3) ?? Data(), encoding: .utf8) }
+            .joined(separator: "\n")
+
+        DispatchQueue.main.async { [weak self] in
+            self?.textLabel.text = message
+        }
     }
 
 
 }
-
