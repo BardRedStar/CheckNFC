e81f0d7ec6364a98d8104a3cdede54c09bb77cc8
BardRedStar <den.kovalev999@gmail.com>
2020-05-18 10:02:42 +0700

    Initial commit

---
+++ CheckNFC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckNFC
+//
+//  Created by Denis Kovalev on 18.05.2020.
+//  Copyright © 2020 Denis Kovalev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
